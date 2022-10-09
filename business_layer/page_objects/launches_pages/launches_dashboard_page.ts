122b614aa016ede0267875294c863f38a414e4f0
Aliaksandr Posakhau <Aliaksandr_Posakhau@epam.com>
2022-10-09 21:15:13 +0300

    added 3 mocha tests from hometask03

--- business_layer/page_objects/launches_pages/launches_dashboard_page.ts
+++ business_layer/page_objects/launches_pages/launches_dashboard_page.ts
@@ -1,3 +1,4 @@
+import { yellow } from "colors";
 import { _ecx, _skx } from "../../../core_layer/utilities/aliases"
 
 export class LaunchesDashboardPage {
@@ -9,5 +10,11 @@ export class LaunchesDashboardPage {
          await _ecx ( `//button[@type='button' and @title='${value}']`);
     }
 
-    
+    public clickTestSuiteByNumber = async ( value : string ) : Promise <void> => {
+        await _ecx ( `//span[text()='${value}']/parent::a/parent::span/parent::div/parent::div/parent::div/parent::div` );
+    } 
+
+    public getLaunchesTableHeaderElement  = async ( value : string ) => {
+        return $(`//span[text()='${value}']`);
+    }
 }
